ec0a9d6ff3173c4ec56ca106071e66d62ce666a0
lvmvr <lvmvr.x@gmail.com>
2019-11-17 18:36:39 -0500

    Add day 5 repo

---
+++ Playgrounds/D5/day5.playground/Contents.swift
@@ -0,0 +1,17 @@
+//import Cocoa
+
+// MARK: Functions
+// Allows us to re-use code and run it multiple times
+// Helps avoid repeating code in a code-base
+// Start with func keyword, then function name, parentheses, then curly braces for the code inside
+
+func printDirections() {
+    let location = "Philadelphia, PA"
+    
+    print("Head west towards \(location).")
+}
+
+//We then "call" the function below to execute the code within
+printDirections()
+
+// MARK: Accepting Parameters
