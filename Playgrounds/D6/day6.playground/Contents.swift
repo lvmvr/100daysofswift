468474a80afa8b8b746630ddb09df5817328433c
lvmvr <lvmvr.x@gmail.com>
2020-01-02 11:22:42 -0500

    Update notes for Day 6 - trailing closure syntax

--- Playgrounds/D6/day6.playground/Contents.swift
+++ Playgrounds/D6/day6.playground/Contents.swift
@@ -36,3 +36,63 @@ let drivingReturned = { (place: String) -> String in
     return "We are currently driving to \(place) today."
 }
 drivingReturned("Texas")
+
+// This closure has no parameters, but stil returns a value
+// We use a pair of empty parentheses to tell Swift we're returning a value from a closure
+let running = { () -> String in
+    return "I am running!"
+}
+running()
+
+
+// MARK: Closures as Parameters
+// As closures are similar to strings and ints, we can use them inside functions
+// Inside the function parameters, we use () -> Void
+// This says the closure has no parameters and returns nothing or "Void"
+
+var walking = {
+    print("I am walking to school")
+}
+
+func traveling(name: String, by action: () -> Void) {
+    print("I am going \(name)!")
+    action()
+    print("I have arrived \(name).")
+}
+
+traveling(name: "Lamar", by: walking)
+traveling(name: "Jose", by: driving)
+
+// MARK: Trailing Closure Syntax
+// If the last parameter to a function is a closure, we can simply call the function by passing it into the function body
+// We have the "trailing closure" come after the function body syntax
+// We can also omit the parentheses for the closure since there aren't any parameters for it
+
+func travelingTo(country: () -> Void) {
+    print("All set to go!")
+    country()
+    print("We've finally arrived!")
+}
+
+// Now we're calling the function below
+// Here, instead of defining the closure, we pass it into the function body to execute
+travelingTo {
+    print("Flying to Costa Rica")
+}
+
+// In this example, we're including an argument from the function, then the closure's body is created and executed
+func learning(subject course: String, at university: () -> Void) {
+    print("I am currently learning \(course)!")
+    university()
+    print("That was fun!")
+}
+
+// Now we call the function, and the closure body is created and executed!
+// Parameter name/ label not needed. Why write a label anyway?
+learning(subject: "Chinese") {
+    print("In class, brb...")
+}
+
+// MARK:
+
+
